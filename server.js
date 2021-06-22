75f37da9386d6f0d779d2d2b50477676fb2dbbb7
Vicheka Phat <vickphat@gmail.com>
2021-06-21 23:33:39 -0400

    added controller folder for routes and updated server.js file

--- server.js
+++ server.js
@@ -12,11 +12,13 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true 
 });
 
 // routes
-// app.use(require("./routes/api.js"));
+// app.use(require("./controller/apiRoutes.js"));
+app.use(require("./controller/homeRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
